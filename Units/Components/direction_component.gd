e47183495fe54e6be500c648481bc2663b9dae05
Nessa Teal <nessa.s.teal@gmail.com>
2025-02-17 01:29:22 +0200

    Fix direction component

--- Units/Components/direction_component.gd
+++ Units/Components/direction_component.gd
@@ -7,18 +7,18 @@ var direction: Vector2 = Vector2.ONE :
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	direction.x = 1
-	direction.y = 0
-	#if Input.is_action_pressed("GoUp"):
-		#direction.y = -1
-	#elif Input.is_action_pressed("GoDown"):
-		#direction.y = 1
-	#else:
-		#direction.y = 0
-		#
-	#if Input.is_action_pressed("GoLeft"):
-		#direction.x = -1
-	#elif Input.is_action_pressed("GoRight"):
-		#direction.x = 1
-	#else:
-		#direction.x = 0
+	#direction.x = 1
+	#direction.y = 0
+	if Input.is_action_pressed("GoUp"):
+		direction.y = -1
+	elif Input.is_action_pressed("GoDown"):
+		direction.y = 1
+	else:
+		direction.y = 0
+	
+	if Input.is_action_pressed("GoLeft"):
+		direction.x = -1
+	elif Input.is_action_pressed("GoRight"):
+		direction.x = 1
+	else:
+		direction.x = 0
