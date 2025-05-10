cefacea3c1bb5b035ca887809bf174a88b3663a2
Nessa Teal <nessa.s.teal@gmail.com>
2025-05-11 00:47:37 +0300

    Fix immolation component

--- Units/Components/immolation_component.gd
+++ Units/Components/immolation_component.gd
@@ -9,13 +9,14 @@ class_name ImmolationComponent
 @onready var timer: Timer = $Timer
 
 func _on_timeout():
-	if immolation > 0:
-		for i in 500:
-			emit_particle(Transform2D(), Vector2(), Color(), Color(), 0)
-		attack_component.perform_attack(immolation / 100 * health_component.max_hp)
-			
-	#elif immolation_storage_coefficient > 0:
-		#stored_immolation = min(stored_immolation + immolation * health_component.max_hp, immolation_storage_coefficient * health_component.max_hp)
+	#amount_ratio += 0.01
+	emitting = true;
+	await get_tree().create_timer(0.05).timeout
+	emitting = false;
+	pass
+	#restart()
+	#for i in 500:
+		#emit_particle(Transform2D(), Vector2(), Color(), Color(), 0)
 
 func reset():
 	immolation = 0.0
