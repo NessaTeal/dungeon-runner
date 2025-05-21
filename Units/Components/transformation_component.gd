e7430cfe2ac411f73ea54f8582d4f63df7c5dbe2
Nessa Teal <nessa.s.teal@gmail.com>
2025-05-21 19:43:27 +0300

    Make sprite a child of the unit scene

--- Units/Components/transformation_component.gd
+++ Units/Components/transformation_component.gd
@@ -1,15 +1,15 @@
 extends Node
 class_name TransformationComponent
 
-@export var player: Player
+@export var sprite: AnimatedSprite2D
 
 @onready var timer: Timer = $ResetTransformationTimer
 
 func transform(form_name: String):
-	var current_frame = player.get_frame()
-	var current_progress = player.get_frame_progress()
-	player.play(form_name)
-	player.set_frame_and_progress(current_frame, current_progress)
+	var current_frame = sprite.get_frame()
+	var current_progress = sprite.get_frame_progress()
+	sprite.play(form_name)
+	sprite.set_frame_and_progress(current_frame, current_progress)
 
 	timer.start()
 
